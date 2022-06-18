dc7b62c37430ef721a1adae60d2701b1701a6e72
kgs90 <kgs90@inbox.ru>
2022-06-18 14:22:53 +0300

    добавлена задача

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите слова через пробел: ");
+String[] size = Console.ReadLine().Split().ToArray();
+
+Console.WriteLine("Введенные слова: ");
+for (int i = 0; i < size.Length; i++) 
+{
+    Console.Write(size[i] + " ");
+}
+Console.WriteLine();
+
+Console.WriteLine("Введенные слова с длинною больше или равной 3: ");
+for (int i = 0; i < size.Length; i++) 
+{
+    if (size[i].Length <= 3) 
+    {
+        Console.Write(size[i] + " ");
+    }
+}
